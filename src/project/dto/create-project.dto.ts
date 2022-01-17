5711fa70afdebfa8d40a8763a8c7309cd23948a1
MahmoudAbuzeed <mahmoudabozeed56@gmail.com>
2022-01-17 22:01:25 +0200

    createing nest application with mysql boilerplate

--- src/project/dto/create-project.dto.ts
+++ src/project/dto/create-project.dto.ts
@@ -1,48 +1,7 @@
-import {
-  IsArray,
-  IsDateString,
-  IsNotEmpty,
-  IsNumber,
-  IsString,
-} from 'class-validator';
-import { Department } from 'src/department/entities/department.entity';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateProjectDto {
   @IsString({ message: 'must be string' }) // override the default message
   @IsNotEmpty({ message: 'must  not be string' })
   name: string;
-
-  @IsString()
-  @IsNotEmpty()
-  reference_id: string;
-
-  @IsNumber()
-  @IsNotEmpty()
-  budget: number;
-
-  @IsString()
-  @IsNotEmpty()
-  type: string;
-
-  @IsDateString()
-  @IsNotEmpty()
-  start_date: Date;
-
-  @IsDateString()
-  @IsNotEmpty()
-  end_date: Date;
-
-  @IsString()
-  @IsNotEmpty()
-  description: string;
-
-  @IsArray()
-  components: any;
-
-  @IsArray()
-  activities: any;
-
-  @IsNumber()
-  @IsNotEmpty()
-  department: Department;
 }
--- src/project/entities/project.entity.ts
+++ src/project/entities/project.entity.ts
@@ -1,12 +1,4 @@
-import {
-  Column,
-  Entity,
-  PrimaryGeneratedColumn,
-  CreateDateColumn,
-  UpdateDateColumn,
-  ManyToOne,
-} from 'typeorm';
-import { Department } from 'src/department/entities/department.entity';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Project {
@@ -15,31 +7,4 @@ export class Project {
 
   @Column({ length: 40 })
   name: string;
-
-  @Column({ length: 40 })
-  reference_id: string;
-
-  @Column()
-  budget: number;
-
-  @Column({ length: 40 })
-  type: string;
-
-  @Column({ nullable: true })
-  start_date: Date;
-
-  @Column({ nullable: true })
-  end_date: Date;
-
-  @Column()
-  description: string;
-
-  @CreateDateColumn()
-  created_at: Date;
-
-  @UpdateDateColumn()
-  updated_at: Date;
-
-  @ManyToOne(() => Department)
-  department: Department;
 }
